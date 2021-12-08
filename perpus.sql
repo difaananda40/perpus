c35d425e2ccb0351d2eb9d048f267b39d84b7420
difaananda40 <difacool40@gmail.com>
2021-12-08 12:31:48 +0800

    Add database

---
+++ perpus.sql
@@ -0,0 +1,203 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 08, 2021 at 04:18 AM
+-- Server version: 5.7.18
+-- PHP Version: 7.4.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perpus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `anggota`
+--
+
+CREATE TABLE `anggota` (
+  `kode_anggota` varchar(30) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `jk` enum('L','P') NOT NULL,
+  `jurusan` varchar(255) NOT NULL,
+  `angkatan` int(11) NOT NULL,
+  `alamat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `anggota`
+--
+
+INSERT INTO `anggota` (`kode_anggota`, `nama`, `jk`, `jurusan`, `angkatan`, `alamat`) VALUES
+('A-1', 'Joni', 'L', 'Perawat', 2022, 'Jl. Polder');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `kode_buku` varchar(30) NOT NULL,
+  `judul` varchar(255) NOT NULL,
+  `penulis` varchar(255) NOT NULL,
+  `penerbit` varchar(255) NOT NULL,
+  `tahun_terbit` int(11) NOT NULL,
+  `kode_rak` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `kode_pinjam` varchar(30) NOT NULL,
+  `tanggal_pinjam` date NOT NULL,
+  `tanggal_kembali` date NOT NULL,
+  `kode_buku` varchar(30) NOT NULL,
+  `kode_petugas` varchar(30) NOT NULL,
+  `kode_anggota` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengembalian`
+--
+
+CREATE TABLE `pengembalian` (
+  `kode_kembali` varchar(30) NOT NULL,
+  `tanggal_kembali` date NOT NULL,
+  `jatuh_tempo` int(11) NOT NULL,
+  `denda` int(11) NOT NULL,
+  `jumlah_denda` int(11) NOT NULL,
+  `kode_buku` varchar(30) NOT NULL,
+  `kode_petugas` varchar(30) NOT NULL,
+  `kode_anggota` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `petugas`
+--
+
+CREATE TABLE `petugas` (
+  `kode_petugas` varchar(30) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `jk` enum('L','P') NOT NULL,
+  `jabatan` varchar(255) NOT NULL,
+  `telp` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `jam_tugas` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `petugas`
+--
+
+INSERT INTO `petugas` (`kode_petugas`, `nama`, `jk`, `jabatan`, `telp`, `alamat`, `jam_tugas`) VALUES
+('P-1', 'faris', 'L', 'Pustakawan', '0821-9221-3969', 'Jl. A.W. Syahranie No. 1', '16:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rak`
+--
+
+CREATE TABLE `rak` (
+  `kode_rak` varchar(30) NOT NULL,
+  `nama_rak` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `anggota`
+--
+ALTER TABLE `anggota`
+  ADD PRIMARY KEY (`kode_anggota`);
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`kode_buku`),
+  ADD KEY `kode_rak` (`kode_rak`);
+
+--
+-- Indexes for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`kode_pinjam`),
+  ADD KEY `kode_petugas` (`kode_petugas`),
+  ADD KEY `kode_anggota` (`kode_anggota`),
+  ADD KEY `kode_buku` (`kode_buku`);
+
+--
+-- Indexes for table `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  ADD PRIMARY KEY (`kode_kembali`),
+  ADD KEY `kode_buku` (`kode_buku`),
+  ADD KEY `kode_petugas` (`kode_petugas`),
+  ADD KEY `kode_anggota` (`kode_anggota`);
+
+--
+-- Indexes for table `petugas`
+--
+ALTER TABLE `petugas`
+  ADD PRIMARY KEY (`kode_petugas`);
+
+--
+-- Indexes for table `rak`
+--
+ALTER TABLE `rak`
+  ADD PRIMARY KEY (`kode_rak`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `buku`
+--
+ALTER TABLE `buku`
+  ADD CONSTRAINT `buku_ibfk_1` FOREIGN KEY (`kode_rak`) REFERENCES `rak` (`kode_rak`);
+
+--
+-- Constraints for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD CONSTRAINT `peminjaman_ibfk_1` FOREIGN KEY (`kode_petugas`) REFERENCES `petugas` (`kode_petugas`),
+  ADD CONSTRAINT `peminjaman_ibfk_2` FOREIGN KEY (`kode_anggota`) REFERENCES `anggota` (`kode_anggota`),
+  ADD CONSTRAINT `peminjaman_ibfk_3` FOREIGN KEY (`kode_buku`) REFERENCES `buku` (`kode_buku`);
+
+--
+-- Constraints for table `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  ADD CONSTRAINT `pengembalian_ibfk_1` FOREIGN KEY (`kode_buku`) REFERENCES `buku` (`kode_buku`),
+  ADD CONSTRAINT `pengembalian_ibfk_2` FOREIGN KEY (`kode_petugas`) REFERENCES `petugas` (`kode_petugas`),
+  ADD CONSTRAINT `pengembalian_ibfk_3` FOREIGN KEY (`kode_anggota`) REFERENCES `anggota` (`kode_anggota`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
